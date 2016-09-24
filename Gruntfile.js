578cef4a25743e75fc8428c3c46d7a47b88e5006
Pedro Martín del Campo González <pedro.martin@lkmx.io>
2016-09-23 22:13:14 -0700

    Add suport for Sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,10 +8,28 @@ module.exports = function(grunt) {
             base: '.'
         }
       }
+    },
+    sass: {                              // Task
+      dist: {                            // Target
+        options: {                       // Target options
+          style: 'expanded'
+        },
+        files: {                         // Dictionary of files
+          'css/app.css': 'scss/app.scss',       // 'destination': 'source'
+        }
+      }
+    },
+    watch : {
+      sass: {
+        files: ['scss/**/*.scss'],
+        tasks: ['sass']
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-connect');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('default', ['connect']);
 
